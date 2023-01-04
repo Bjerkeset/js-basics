f5ec09b3df9467e66b39d5d1e53d830b5d135a74
Bjerkeset <112175751+Bjerkeset@users.noreply.github.com>
2023-01-04 12:57:28 +0100

    second-commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+
+let bob = "human" + " " + "man";
+console.log(bob);
+let alice = "human" + " " + "woman";
+console.log(alice);
+var humans = bob + alice;
+console.log(humans);
+console.log(alice + bob);
+
+
+var titleSelector = "get element by query selector";
+var titleId = "get element by id";
+document.querySelector("h1").innerHTML = titleSelector;
+document.getElementById("h1").innerHTML = titleId;
+
