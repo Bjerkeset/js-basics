afdf0eb540c76590ad4d57f14f629fe79f40bb63
Bjerkeset <112175751+Bjerkeset@users.noreply.github.com>
2023-01-06 11:25:06 +0100

    ...

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 
-let bob = "human" + " " + "man";
-console.log(bob);
-let alice = "human" + " " + "woman";
-console.log(alice);
-var humans = bob + alice;
-console.log(humans);
-console.log(alice + bob);
+// let bob = "human" + " " + "man";
+// console.log(bob);
+// let alice = "human" + " " + "woman";
+// console.log(alice);
+// var humans = bob + alice;
+// console.log(humans);
+// console.log(alice + bob);
 
 
-var titleSelector = "get element by query selector";
-var titleId = "get element by id";
-document.querySelector("h1").innerHTML = titleSelector;
-document.getElementById("h1").innerHTML = titleId;
+// var titleSelector = "get element by query selector";
+// var titleId = "get element by id";
+// document.querySelector("h1").innerHTML = titleSelector;
+// document.getElementById("h1").innerHTML = titleId;
+
+
+
+//LOOPS
+//ONE TO SEVEN
+for (let oneToSeven = 1; oneToSeven <= 7; oneToSeven++) {
+    console.log(oneToSeven);
+  }
+
+//10 TO 1
+for(let tenToOne = 10; tenToOne >= 1; tenToOne--) {
+    console.log(tenToOne)
+}
+
+// 10 TO 20
+for(let tenToTwenty = 10; tenToTwenty <=20; tenToTwenty++){
+    
+    if (tenToTwenty % 2 === 1) {
+        console.log(tenToTwenty)
+    } else {}
+}
+
+
 
